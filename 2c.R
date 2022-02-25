275c7db1180b736d88a9b4f8a52ff30c1797f9ee
maxfaber <maxfaber@quicknet.nl>
2022-02-25 11:47:31 +0100

    Removed unnecessary code from 2c

--- 2c.R
+++ 2c.R
@@ -9,21 +9,12 @@ estimator = x$parameters
 x$interval$limits
 
 
-# bootstrap CI
 B=1000
 tstar=numeric(B)
-T1 = median(dataset$seeded)
 for (i in 1:B){
   xstar = sample(dataset$seeded, replace=TRUE)
   tstar[i] = median(xstar)
 }
-hist(tstar)
-qqnorm(tstar)
-?rexp
 gen_exp = rexp(B, rate=estimator)
-gen_exp
 t.test(tstar, gen_exp,mu=0, alternative="two.sided", conf.level=0.95)
-
-
 ks.test(tstar, "pexp", estimator)
-
