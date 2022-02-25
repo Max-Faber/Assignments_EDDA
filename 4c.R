c6929ed51240412a410802e3bf301d66e21c33a3
maxfaber <maxfaber@quicknet.nl>
2022-02-25 16:01:31 +0100

    Few changes for the report

--- 4c.R
+++ 4c.R
@@ -1,2 +1,17 @@
 setwd("S:/Shared/Documents/Study/VU/MSc Year 1/Experimental Design and Data Analysis/Assignments/Assignment 1")
 dataset = read.table('./Datasets/hemoglobin.txt', header = TRUE)
+rate_factor = as.factor(dataset$rate)
+method_factor = as.factor(dataset$method)
+lin_mod = lm(dataset$hemoglobin~rate_factor+method_factor)
+
+data=c()
+i=0
+for (x in 1:length(dataset$hemoglobin)){
+  if (dataset$method[x] == 'A' && dataset$rate[x]==3){
+    i=i+1
+    data[i] = dataset$hemoglobin[x]
+  }
+}
+data
+library(Rmisc)
+CI(data, ci=0.95)
