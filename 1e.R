136d2c44e041a0e711d74aa957a62c7c4ed653ec
maxfaber <maxfaber@quicknet.nl>
2022-02-28 21:58:01 +0100

    Changed straategy in 1e from proportional test to Fisher's test

--- 1e.R
+++ 1e.R
@@ -1,1 +1,5 @@
-prop.test(x=c(3, 2), n=c(7,8), alternative='two.sided')
+# prop.test(x=c(3, 2), n=c(7,8), alternative='two.sided')
+arr = c(15.4, 17.9, 19.0, 0.5, 15.9, 2.7, 6.2, 2.5, 4.7, 6.9, 10.8, 24.3, 5.6, 23.0, 10.7)
+df = data.frame("> 15.5 minutes" = c(3, 2), "< 15.5 minutes" = c(4, 6),
+                row.names=c("Men", "Women"))
+fisher.test(df)
